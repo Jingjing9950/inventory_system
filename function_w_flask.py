a0ea731edbfa9a8b41bb32fd72fc114aa0d7a415
Jingjing <32927622+Jingjing9950@users.noreply.github.com>
2018-09-15 16:27:04 +0800

    Update functions

--- function_w_flask.py
+++ function_w_flask.py
@@ -11,8 +11,6 @@ df_inventory = pd.DataFrame({'Shop_id':[1001,1001,1002,1002,1003,1003],'Item_id'
 df_store.index = df_store.index + 1
 df_item.index = df_item.index + 1
 
-summary = df_inventory.groupby(['Shop_id', 'Item_id'], as_index=False)['quantity'].sum()
-
 @app.route("/new-shop/<shop_name>")
 def add_new_shop(shop_name):
     new_row_index = len(df_store.index)+1
@@ -37,12 +35,15 @@ def get_name_by_itemid(item_id):
 
 @app.route("/purchase/<int:shop_id>/<int:item_id>/<int:volume>")
 def add_quan_by_id(shop_id, item_id,volume):
-    df_inventory.loc[(df_inventory['Shop_id'] == shop_id) & (df_inventory['Item_id'] == item_id),'quantity'] += volume
+    new_row_index = len(df_inventory)
+    df_inventory.loc[new_row_index] = [item_id,shop_id,volume]
+    #df_inventory.loc[(df_inventory['Shop_id'] == shop_id) & (df_inventory['Item_id'] == item_id),'quantity'] += volume
     return df_inventory.to_json()
 
 @app.route("/find-inventory/<int:shop_id>/<int:item_id>")
 def get_quan_by_id(shop_id, item_id):
-    find_inventory = str(df_inventory[(df_inventory.Shop_id== shop_id) & (df_inventory.Item_id == item_id)].quantity.item())
+    summary = df_inventory.groupby(['Shop_id', 'Item_id'], as_index=False)['quantity'].sum()
+    find_inventory = str(summary[(summary.Shop_id== shop_id) & (summary.Item_id == item_id)].quantity.item())
     return find_inventory
 
 @app.route("/find-inventory-item/<int:item_id>")
@@ -52,6 +53,7 @@ def get_quan_all_by_id(item_id):
 
 @app.route("/find-null-item/<int:shop_id>")
 def get_item_name_null_shop_id(shop_id):
+    summary = df_inventory.groupby(['Shop_id', 'Item_id'], as_index=False)['quantity'].sum()
     sub_list_by_shop_id = summary.loc[summary['Shop_id'] == shop_id]
     match_result = pd.merge(df_item,sub_list_by_shop_id, on = 'Item_id', how='outer')
     find_null_item = pd.Series(dict(match_result[(match_result['quantity'] < 1)|(match_result['quantity'].isnull())].Item_name.items()))
@@ -62,6 +64,7 @@ def get_item_name_null_shop_id(shop_id):
 
 @app.route("/find-max-item-shop")
 def get_max_quan_by_shop():
+    summary = df_inventory.groupby(['Shop_id', 'Item_id'], as_index=False)['quantity'].sum()
     find_all_max_shop = summary[summary.groupby(['Shop_id'])['quantity'].transform(max) == summary['quantity']]
     find_shop_name = pd.merge(find_all_max_shop, df_store, on = 'Shop_id', how='left')
     find_item_name = pd.merge(find_shop_name, df_item, on = 'Item_id', how='left')
@@ -70,7 +73,7 @@ def get_max_quan_by_shop():
     string += "<body>"
     string += "<ul>"
     for q in range(len(find_item_name['Shop_name'])):
-        string += str("<li>" +find_item_name['Shop_name'][q]) + " - " + str(find_item_name['Item_id'][q]) + " - " + str(find_item_name['quantity'][q]) + "</li>\n"
+        string += str("<li>" +find_item_name['Shop_name'][q]) + " - " + str(find_item_name['Item_name'][q]) + " - " + str(find_item_name['quantity'][q]) + "</li>\n"
     string += "</ul>"
     string += "</body>"
     string += "</html>"
@@ -79,4 +82,3 @@ def get_max_quan_by_shop():
 
 if __name__=="__main__":
     app.run(debug = True)
-
